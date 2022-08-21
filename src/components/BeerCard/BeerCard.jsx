f2821e205a4988600fb6c530fb7b2f337cd80e24
Cody Crites <cody91494@hotmail.com>
2022-08-21 15:59:33 -0500

    b

--- src/components/BeerCard/BeerCard.jsx
+++ src/components/BeerCard/BeerCard.jsx
@@ -1,12 +1,13 @@
 import "./BeerCard.scss"
 
-const BeerCard = ({ name, image, abv, tagline }) => {
+const BeerCard = ({ name, image, abv, tagline, ph}) => {
     return (
       <div className="beer-card">
         <img className="beer-card__image" src={image} alt={name} />
         <div className="beer-card__content">
           <h2 className="beer-card__heading">{name}</h2>
           <p>ABV: {abv}</p>
+          <p>pH: {ph}</p>
           <p>{tagline}</p>
         </div>
       </div>
