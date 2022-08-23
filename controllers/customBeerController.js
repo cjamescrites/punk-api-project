bd408e7b86873000fce51745d872328ac93eaca4
Cody Crites <cody91494@hotmail.com>
2022-08-23 15:39:23 -0500

    added custom beer controller and beer

---
+++ controllers/customBeerController.js
@@ -0,0 +1,11 @@
+const customBeer = {
+    id: 1994,
+    name: "Cody's Tex-Mex Brew",
+    tagline: "Taste you'll have to remember like the Alamo",
+    first_brewed: "09/1994",
+    description: "IPA brewed with American hops, packing a strong punch with added serranos with agave for a sweet after taste.",
+    image_url: "https://www.thecarycompany.com/media/catalog/product/3/0/30blab_1.jpg",
+    abv: 7,
+    ph: 5
+
+}
