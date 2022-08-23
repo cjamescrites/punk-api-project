45aadbbf51de66c2a695afa4d685fff7c7c4300a
Cody Crites <cody91494@hotmail.com>
2022-08-22 20:49:39 -0500

    edited all beers

--- src/containers/AllBeers/AllBeers.jsx
+++ src/containers/AllBeers/AllBeers.jsx
@@ -4,7 +4,7 @@ const AllBeers = (props) => {
     const {beers} = props;
     
     const mappedBeers = beers.map((beer) => { 
-        return <BeerCard key={beer.id} name={beer.name} image={beer.image_url} abv={beer.abv} ph={beer.ph} year={beer.first_brewed} tagline={beer.tagline} />
+        return <BeerCard key={beer.id} name={beer.name} image={beer.image_url} abv={beer.abv} ph={beer.ph} year={beer.first_brewed} tagline={beer.tagline} description={beer.description} />
       });
 
     return (
