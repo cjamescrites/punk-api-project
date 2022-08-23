8a2d871989462dff7e856068b96688477c91d206
Cody Crites <cody91494@hotmail.com>
2022-08-22 20:49:49 -0500

    edited explore beers

--- src/containers/ExploreBeers/ExploreBeers.jsx
+++ src/containers/ExploreBeers/ExploreBeers.jsx
@@ -3,24 +3,18 @@ import SearchBar from "../../components/SearchBar/SearchBar";
 import BeerCard from "../../components/BeerCard/BeerCard"
 
 const ExploreBeers = (props) => {
-  const { beers } = props;
-  const [searchTerm, setSearchTerm] = useState("");
+  const {beers, searchTerm, setSearchTerm} = props;
+
 
   const handleInput = (event) => {
     const cleanInput = event.target.value.toLowerCase();
     setSearchTerm(cleanInput);
   };
 
-  const filteredBeers = beers.filter((beer) => {
-    const beerTitleLower = beer.name.toLowerCase();
-
-    return beerTitleLower.includes(searchTerm);
-  });
 
   return (
     <>
       <SearchBar
-        label={"beers"}
         searchTerm={searchTerm}
         handleInput={handleInput}
       />
